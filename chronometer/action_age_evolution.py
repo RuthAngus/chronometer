bdbbae15ae776397605f37f7a52b0a693b321d6e
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-02-10 15:49:46 -0500

    words on the subway

--- chronometer/action_age_evolution.py
+++ chronometer/action_age_evolution.py
@@ -7,6 +7,7 @@ import pandas as pd
 from galpy.potential import MWPotential2014
 from galpy.actionAngle import actionAngleStaeckel, actionAngleAdiabatic
 from galpy.df import quasiisothermaldf
+aA = actionAngleStaeckel(pot=MWPotential2014, delta=0.45, c=True)
 
 
 def calc_dispersion(time, sz0, t1, tm, beta, R0, Rc, Rsz):
@@ -24,6 +25,12 @@ if __name__ == "__main__":
     Rc = 1.
     hsz = 9.
     hsr = 9.
+    solar_radius = 8.
+    hr = 2.68/solar_radius
+
+    # Today
+    sr = 34.
+    sz = 25.1
 
     sigma_z = calc_dispersion(time, sz0, t1, tm, beta, R0, Rc, hsz)
     sigma_r = calc_dispersion(time, sr0, t1, tm, beta, R0, Rc, hsr)
@@ -33,24 +40,31 @@ if __name__ == "__main__":
     plt.savefig("test")
 
     # Bovy example
-    # aA = actionAngleAdiabatic(pot=MWPotential2014, c=True)
-    # hr = 1/3.  # radial scale length
-    # sr = .2  # radial velocity dispersion at the solar radius (time dependent)
-    # sz = .1  # vertical velocity dispersion at the solar radius (time dependent)
-    # hsr = 1.  # radial-velocity-dispersion scale length
-    # hsz = 1.  # vertial-velocity-dispersion scale length
+    aA = actionAngleAdiabatic(pot=MWPotential2014, c=True)
+    hrb = 1/3.  # radial scale length
+    srb = .2  # radial velocity dispersion at the solar radius (time dependent)
+    szb = .1  # vertical velocity dispersion at the solar radius (time dependent)
+    hsrb = 1.  # radial-velocity-dispersion scale length
+    hszb = 1.  # vertial-velocity-dispersion scale length
 
-    aA = actionAngleStaeckel(pot=MWPotential2014, delta=0.45, c=True)
-    solar_radius = 8.
-    hr = 2.68/solar_radius
-    # sr = 34.
-    # sz = 25.1
+    print(sr, srb)
+    qdf = quasiisothermaldf(hr, srb, szb, hsrb, hszb, pot=MWPotential2014,
+                            aA=aA, cutcounter=True)
+    print(qdf.meanjz(1., .125))
+    assert 0
 
     meanjzs = np.ones(len(sigma_z))
     for i, sz in enumerate(sigma_z):
+        print(i)
         qdf = quasiisothermaldf(hr, sigma_r[i], sz, hsr, hsz,
                                 pot=MWPotential2014, aA=aA, cutcounter=True)
-        meanjzs.append(qdf.meanjz(1., .125))
-    print(qdf.estimate_hr(1.))
-    print(qdf.meanvz(1., 0.))
-    print(qdf.meanjz(1., .125))
+        print(sz, sigma_r[i])
+        mj = qdf.meanjz(1., .125)
+        print(mj)
+        meanjzs[i] = mj
+
+    plt.clf()
+    plt.plot(time, meanjzs)
+    plt.xlabel("Time (Gyr)")
+    plt.ylabel("Mean(Jz)")
+    plt.savefig("meanjz_evolution")
