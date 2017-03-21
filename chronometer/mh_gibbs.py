f9b2e05a8ed8e053c146be198bedf3f43d6d51c1
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-03-21 14:40:24 -0400

    removing original versions

--- chronometer/mh_gibbs.py
+++ chronometer/mh_gibbs.py
@@ -15,45 +15,18 @@ def lnlike(par, x, y, yerr):
     return sum(-.5*((y_mod - y)/yerr)**2)
 
 
-def metropolis_hastings(r, N, s, t, x, y, yerr):
-    """
-    params:
-    -------
-    r: (tuple or list or array)
-        The parameters/position in probability space.
-    N: (int)
-        Number of samples.
-    s: (int)
-        Thinning index
-
-    """
-    print("inits = ", r)
-    p = np.exp(lnlike(r, x, y, yerr))  # Initial probability.
-    samples = []
-    for i in np.arange(N):
-        rn = r + np.random.normal(size=2)*t  # Take a step in parameter space.
-        pn = np.exp(lnlike(r, x, y, yerr))  # calculate the probability.
-        if pn >= p:  # If the new probability is greater than the previous p
-            p = pn  # The probability is now the new one.
-            r = rn  # adopt the new parameters.
-        else:
-            u = np.random.rand()  # Otherwise, random number between 0 and 1.
-            if u < pn/p:  # if less than the ratio of new p to old p:
-                p = pn  # prob is now new prob.
-                r = rn  # adopt the new parameters.
-        samples.append(r)
-    return np.array(samples)
-
 def MH(par, N, t, x, y, yerr):
     """
     params:
     -------
     par: (list)
         The parameters.
-    args: *args
-        Arguments to parse to the lnlike.
+    x, y, yerr: (arrays)
+        The data
     N: (int)
         Number of samples.
+    t: (float)
+        The std of the proposal distribution.
     """
     samples = np.zeros((N, len(par)))
     for i in range(N):
@@ -72,7 +45,7 @@ def MH(par, N, t, x, y, yerr):
 
 if __name__ == "__main__":
 
-    # Test on a straight line model
+    # Straight line model
     x = np.arange(0, 10, .1)
     err = 2.
     yerr = np.ones_like(x) * err
@@ -119,33 +92,23 @@ if __name__ == "__main__":
     plt.axvline(.7, color="HotPink")
     plt.savefig("emcee_corner")
 
-    print("Running Metropolis Hastings 1")
+    print("Running Metropolis Hastings")
     N = 1000000  # N samples
-    s = 1  #
     pars = [.5, 2.5]  # initialisation
     t = .01
-    samples1 = metropolis_hastings(pars, N, s, t, x, y, yerr)
-    print(np.shape(samples1), "metropolis hastings")
+    samples = MH(pars, N, t, x, y, yerr)
 
-    print("Running Metropolis Hastings 2")
-    samples2 = MH(pars, N, t, x, y, yerr)
-    print(np.shape(samples2), "mh")
-
+    # Compare results
     plt.clf()
-    print(np.shape(flat))
-    plt.hist(samples1[:, 0], 10, normed=True, alpha=.5, label="mh1")
-    plt.hist(samples2[:, 0], 10, normed=True, alpha=.5, label="mh2")
+    plt.hist(samples[:, 0], 10, normed=True, alpha=.5, label="mh2")
     plt.hist(flat[:, 0], 10, normed=True, alpha=.5, label="emcee")
     plt.legend()
     plt.axvline(.7, color=".7", ls="--")
     plt.xlim(-1, 2)
     plt.savefig("corner")
 
+    # Plot 2d posteriors
     fig = corner.corner(flat)
     fig.savefig("triangle_emcee")
-
-    fig = corner.corner(samples2)
+    fig = corner.corner(samples)
     fig.savefig("triangle_mh")
-
-    fig = corner.corner(samples1)
-    fig.savefig("triangle_mh1")
