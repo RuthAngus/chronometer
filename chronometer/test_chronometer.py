98112fc295a640a81ae3a7b7979e579171fb5f46
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-04-06 15:05:54 -0400

    better data_file test

--- chronometer/test_chronometer.py
+++ chronometer/test_chronometer.py
@@ -31,48 +31,23 @@ class ChronometerTestCase(unittest.TestCase):
         self.assertTrue(b)
 
     def test_data_file_validity(self):
+        """
+        Test that all the values in the input files are within reasonable
+        ranges.
+        """
         DATA_DIR = "/Users/ruthangus/projects/chronometer/chronometer/data"
         d = pd.read_csv(os.path.join(DATA_DIR, "data_file.csv"))
         bounds = [[0, 13.7], [0, 10], [0, 200], [0, 100], [-10, 10], [0, 10],
                   [0, 30], [0, 10], [0, 30], [0, 10], [0, 30], [0, 10],
-                  [0, 30], [0, 10], [0, 30], [0, 10], [0, 1e5], [0, 100],
-                  [1000, 10000], [1, 1000], [2, 6], [0, 3], [-4, 6], [0, 10]]
+                  [0, 30], [0, 10], [0, 30], [0, 10], [0, 30], [0, 10],
+                  [0, 1e5], [0, 100], [1000, 10000], [1, 1000], [2, 6],
+                  [0, 3], [-4, 6], [0, 10]]
 
-        print(np.shape(d))
-        print(len(bounds))
-        assert 0
         b = True
         for i, bound in enumerate(bounds):
-            b += (bound[0] < d[:, i][np.isfinite(d[:, i])]) & \
-                (d[:, i][np.isfinite(d[:, i])] < bound[1])
-
-        # b = (0 < d.age[np.isfinite(d.age)]) & \
-        #     (d.age[np.isfinite(d.age)] < 13.7) \
-        #     & (0 < d.age_err[np.isfinite(d.age_err)]) & \
-        #     (d.age_err[np.isfinite(d.age_err)] < 10) & \
-        #     (0 < d.period[np.isfinite(period)]) & \
-        #     (d.period[np.isfinite(period)] < 200) & \
-        #     (0 < d.period_err[np.isfinite(period_err)]) & \
-        #     (d.period_err[np.isfinite(period_err)] < 100) & \
-        #     (-10 < d.bv[np.isfinite(d.bv)]) & \
-        #     (d.bv[np.isfinite(d.bv)] < 10) & \
-        #     (0 < d.bv_err[np.isfinite(d.bv_err)]) & \
-        #     (d.bv_err[np.isfinite(d.bv_err)] < 10) & \
-        #     # (0 < d.v) & (d.v < 30) & (0 < d.v_err) & (d.v_err < 10) & \
-        #     # (0 < d.ks) & (d.ks < 30) & (0 < d.ks_err) & (d.ks_err < 10) & \
-        #     # (0 < d.vk) & (d.vk < 30) & (0 < d.vk_err) & (d.vk_err < 10) & \
-        #     # (0 < d.j) & (d.j < 30) & (0 < d.j_err) & (d.j_err < 10) & \
-        #     # (0 < d.h) & (d.h < 30) & (0 < d.h_err) & (d.h_err < 10) & \
-        #     # (0 < d.k) & (d.k < 30) & (0 < d.k_err) & (d.k_err < 10) & \
-        #     # (0 < d.parallax) & (d.parallax < 1e5) & (0 < d.parallax_err) & \
-        #     # (d.parallax_err < 100) &\
-        #     # (1000 < d.Teff) & (d.Teff < 10000) & (1 < d.Teff_err) & \
-        #     # (d.Teff_err < 1000) & \
-        #     # (2. < d.logg) & (d.logg < 6) & (0 < d.logg_err) & \
-        #     # (d.logg_err < 3) &\
-        #     # (-4. < d.feh) &(d.feh < 6) & (0 < d.feh_err) & (d.feh_err < 10)
-
-        # self.assertTrue(b.all())
+            b += (bound[0] < d.iloc[:, i][np.isfinite(d.iloc[:, i])]) & \
+                (d.iloc[:, i][np.isfinite(d.iloc[:, i])] < bound[1])
+        self.assertTrue(b.all())
 
 
 if __name__ == "__main__":
