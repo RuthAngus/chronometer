9509acb26bda5f766e77c9a6de9e1d84ddcd1876
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-04-17 18:43:41 -0400

    testing transform parameters

--- chronometer/test_chronometer.py
+++ chronometer/test_chronometer.py
@@ -5,22 +5,17 @@ Unit tests for chronometer.py
 import os
 import numpy as np
 import unittest
-from chronometer import lnprior, lnprob, replace_nans_with_inits, \
-    make_param_dict, get_inits, get_mod_list
+import gibbs_chronometer as gc
 import pandas as pd
 from isochrones import StarModel
 from isochrones.mist import MIST_Isochrone
+import priors
 
 
 class ChronometerTestCase(unittest.TestCase):
     """
     Tests for chronometer.py
     """
-    def __init__(self):
-        DATA_DIR = "/Users/ruthangus/projects/chronometer/chronometer/data"
-        self.d = pd.read_csv(os.path.join(DATA_DIR, "data_file.csv"))
-        self.p0 = get_inits(d)
-        self.mods = get_mod_list(d)
 
     def test_data_file_validity(self):
         """
@@ -40,30 +35,70 @@ class ChronometerTestCase(unittest.TestCase):
         b = True
         for i, bound in enumerate(bounds):
             b += (bound[0] < \
-                  self.d.iloc[:, i][np.isfinite(self.d.iloc[:, i])]) & \
-                (self.d.iloc[:, i][np.isfinite(self.d.iloc[:, i])] <
+                  d.iloc[:, i][np.isfinite(d.iloc[:, i])]) & \
+                (d.iloc[:, i][np.isfinite(d.iloc[:, i])] <
                  bound[1])
         self.assertTrue(b.all())
 
-    def test_lnprobs(self):
+    def test_lnprob_returns_finite(self):
         """
         Make sure lnprob returns finite.
         """
-        args = [self.mods, self.d.period.values, self.d.period_err.values,
-                self.d.bv.values, self.d.bv_err.values]
-        self.assertTrue(np.isfinite((lnprob(p0, *args))))
+        args = [mods, d.period.values, d.period_err.values,
+                d.bv.values, d.bv_err.values, "both"]
+        self.assertTrue(np.isfinite((gc.lnprob(p0, *args))))
 
-    def test_lnprior(self):
+    def test_iso_single_lnlike_returns_finite(self):
+        """
+        Make sure iso_single_lnlike_returns_finite.
+        """
+        self.assertTrue(np.isfinite((gc.iso_single_lnlike(one_star_pars,
+                                                          mods[0]))))
+
+    def test_lnprior_returns_finite(self):
         """
         Make sure lnprior returns a finite number.
         """
-        self.assertTrue(np.isfinite(lnprior(self.p0)))
+        self.assertTrue(np.isfinite(gc.lnprior(p0)))
 
-    # def test_gc_lnlike(self):
-    #     params = np.array([.7725, .601, .5189, 0., np.log(4.56), 0., 100.,
-    #                        0.])
-    #     print(gc_lnlike(params. 26, 1., .65, ,.01))
+    def test_parameter_assignment_reasonable(self):
+        Nstars = 3
+        N, ln_mass, ln_age, feh, ln_distance, Av = \
+        gc.parameter_assignment(p0[3:], "iso")
+        self.assertTrue(N == len(ln_mass))
+        self.assertTrue(type(N) == int)
+        self.assertTrue(Nstars == N)
 
+        N, ln_mass, ln_age, feh, ln_distance, Av = \
+            gc.parameter_assignment(p0, "both")
+        self.assertTrue(N == len(ln_mass))
+        self.assertTrue(type(N) == int)
+        self.assertTrue(Nstars == N)
+
+    def test_transform_parameters_correct(self):
+        Nstars = 3
+        p, N = gc.transform_parameters(p0[3:], "iso", False)
+        self.assertTrue(len(p) == Nstars*5)
+        self.assertTrue(Nstars == N)
+        p, N = gc.transform_parameters(p0, "iso", True)
+        self.assertTrue(len(p == Nstars*5))
+        self.assertTrue(Nstars == N)
+        pars, lnages = gc.transform_parameters(p0[:6], "gyro", False)
+        self.assertTrue(len(pars) == 3)
+        self.assertTrue(len(lnages) == Nstars)
+        pars, lnages = gc.transform_parameters(p0, "gyro", True)
+        self.assertTrue(len(pars) == 3)
+        self.assertTrue(len(lnages) == Nstars)
+
+    def test_iso_lnprior_one_star(self):
+        gc.iso_lnprior(one_star_pars)
 
 if __name__ == "__main__":
+
+    # Global variables.
+    DATA_DIR = "/Users/ruthangus/projects/chronometer/chronometer/data"
+    d = pd.read_csv(os.path.join(DATA_DIR, "data_file.csv"))
+    p0, mods = gc.pars_and_mods(DATA_DIR)
+    one_star_pars = [np.log(1.), np.log(4.), 0., np.log(1000.), 0.]
+
     unittest.main()
