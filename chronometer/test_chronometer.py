0ecf8228937dad834400be962d85d1e54283f9ca
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-04-18 16:35:36 -0400

    testing metropolis hastings

--- chronometer/test_chronometer.py
+++ chronometer/test_chronometer.py
@@ -25,6 +25,7 @@ class ChronometerTestCase(unittest.TestCase):
         j,j_err,h,h_err,k,k_err,parallax,parallax_err,Teff,Teff_err,logg,
         logg_err,feh,feh_err,mass,mass_err,Av,Av_err
         """
+        print("Test 1")
         bounds = [[0, 13.7], [0, 10], [0, 200], [0, 100], [-10, 10], [0, 10],
                   [0, 30], [0, 10], [0, 30], [0, 10], [0, 30], [0, 10],
                   [0, 30], [0, 10], [0, 30], [0, 10], [0, 30], [0, 10],
@@ -44,6 +45,7 @@ class ChronometerTestCase(unittest.TestCase):
         """
         Make sure lnprob returns finite.
         """
+        print("Test 2")
         args = [mods, d.period.values, d.period_err.values,
                 d.bv.values, d.bv_err.values, "both"]
         self.assertTrue(np.isfinite((gc.lnprob(p0, *args))))
@@ -52,6 +54,7 @@ class ChronometerTestCase(unittest.TestCase):
         """
         Make sure iso_single_lnlike_returns_finite.
         """
+        print("Test 3")
         self.assertTrue(np.isfinite((gc.iso_single_lnlike(one_star_pars,
                                                           mods[0]))))
 
@@ -59,9 +62,14 @@ class ChronometerTestCase(unittest.TestCase):
         """
         Make sure lnprior returns a finite number.
         """
+        print("Test 3")
         self.assertTrue(np.isfinite(gc.lnprior(p0)))
 
     def test_parameter_assignment_reasonable(self):
+        """
+        Test the parameter assignment function.
+        """
+        print("Test 4")
         Nstars = 3
         N, ln_mass, ln_age, feh, ln_distance, Av = \
         gc.parameter_assignment(p0[3:], "iso")
@@ -80,6 +88,10 @@ class ChronometerTestCase(unittest.TestCase):
         self.assertTrue(type(N) == int)
 
     def test_transform_parameters_correct(self):
+        """
+        Test the transform parameters function.
+        """
+        print("Test 5")
         Nstars = 3
         p, N = gc.transform_parameters(p0[3:], "iso", False)
         self.assertTrue(len(p) == Nstars*5)
@@ -95,7 +107,26 @@ class ChronometerTestCase(unittest.TestCase):
         self.assertTrue(len(lnages) == Nstars)
 
     def test_iso_lnprior_one_star(self):
+        """
+        Test the iso_lnprior function works on one star.
+        """
+        print("Test 6")
         self.assertTrue(np.isfinite(gc.iso_lnprior(np.array(one_star_pars))))
+
+    def test_mh_correct_size(self):
+        """
+        Test the Metropolis Hastings function returns the right size of
+        samples.
+        """
+        print("Test 7")
+        i, g, star_number = True, True, None
+        params, mods = gc.pars_and_mods(DATA_DIR)
+        p0, args = gc.assign_args(params, mods, d, i, g, star_number)
+        N, nd = 100, 3 + 5*3
+        nsteps, ndim = np.shape(gc.MH(p0, N, .01, *args))
+        self.assertTrue(nsteps == N)
+        self.assertTrue(ndim == nd)
+
 
 if __name__ == "__main__":
 
