7d436e893a62dcc819b14948b19839b772ce40fa
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-04-21 08:45:35 -0400

    exp ratio, tuning params

--- chronometer/chronometer.py
+++ chronometer/chronometer.py
@@ -40,56 +40,53 @@ def lnlike(params, *args):
         The parameters to vary.
     """
     mods, period, period_errs, bv, bv_errs, par_inds = args
-    N = int((len(params) - 3)/5.)
-    print(N, "N")
+    N = len(period)
     gyro_lnlike, iso_lnlike = 0, 0
     if par_inds[0] == 0 and par_inds[1] == 1 and par_inds[2] == 2: # if gyro.
-        if len(par_inds) == 3 + N:  # If just gyro.
-            gyro_lnlike =  sum(-.5*((period - gyro_model(params[par_inds],
-                                                bv))/period_errs)**2)
-        elif len(par_inds) == 3 + 5*N:  # If gyro and all stars.
+
+        # If just gyro.
+        if len(par_inds) == 3 + N:
+            gyro_lnlike = sum(-.5*((period - gyro_model(params, bv))
+                                    /period_errs)**2)
+
+        # If gyro and all stars.
+        elif len(par_inds) == 3 + 5*N:
             g_par_inds = np.concatenate((par_inds[:3], par_inds[3+N:3+2*N]))
             gyro_lnlike =  sum(-.5*((period - gyro_model(params[g_par_inds],
                                                 bv))/period_errs)**2)
             p = params*1
-            p[3:3+N] = np.exp(p[:N])
-            p[3+N:3+2*N] = np.log10(1e9*np.exp(p[N:2*N]))
-            p[3+3*N:3+4*N] = np.exp(p[3*N:4*N])
-            iso_lnlike = [mods[i].lnlike(params[par_inds][3+i::N]) for i in
-                          range(len((mods)))]
-        else:
-            assert len(par_inds) - 3 - N == 0  # Not gyro + single stars.
-    else:  # If not gyro but single stars
-        print("yes")
+            p[3:3+N] = np.exp(p[3:3+N])
+            p[3+N:3+2*N] = np.log10(1e9*np.exp(p[3+N:3+2*N]))
+            p[3+3*N:3+4*N] = np.exp(p[3+3*N:3+4*N])
+            iso_lnlike = sum([mods[i].lnlike(p[3+i::N]) for i in
+                              range(len(mods))])
+
+    # If not gyro but single stars
+    else:
         mod_inds = par_inds[0] - 3
-        print(mod_inds)
         ms = mods[mod_inds]
-        print(ms)
-        # try:
-        #     if len(ms) > 1:
-        #         print(params[par_inds][i::N])
-        #         iso_lnlike = [ms[i].lnlike(params[par_inds][i::N]) for i in
-        #                     range(len((ms)))]
-        # except TypeError:
-        print(params[par_inds])
-        iso_lnlike = ms.lnlike(params[par_inds])
+        p = params*1
+        p[0] = np.exp(p[0])
+        p[1] = np.log10(1e9*np.exp(p[1]))
+        p[3] = np.exp(p[3])
+        iso_lnlike = ms.lnlike(p)
     return gyro_lnlike + iso_lnlike
 
 
 def lnprior(params):
     """
-    lnprior on the parameters when both iso and gyro parameters are being
-    inferred.
+    lnprior on all parameters.
     """
-    Nstars = int(len(params - 3)/5)
-
+    N = int(len(params - 3)/5)  # Number of stars
     age_prior = sum([np.log(priors.age_prior(np.log10(1e9*np.exp(i))))
-                     for i in params[N:2*N]])
-    feh_prior = sum([np.log(priors.feh_prior(i)) for i in params[2*N:3*N]])
+                     for i in params[3+N:3+2*N]])
+    feh_prior = sum([np.log(priors.feh_prior(i)) for i in
+                     params[3+2*N:3+3*N]])
     distance_prior = sum([np.log(priors.distance_prior(np.exp(i))) for i
-                          in params[3*N:4*N]])
+                          in params[3+3*N:3+4*N]])
     g_prior = priors.lng_prior(params[:3])
     m = (-20 < params) * (params < 20)  # Broad bounds on all params.
+    Av = params[3+4*N:3+5*N]
     mAv = (0 <= Av) * (Av < 1)
     if sum(m) == len(m) and sum(mAv) == len(mAv):
         return age_prior + feh_prior + distance_prior + g_prior #+ Av_prior
@@ -105,8 +102,7 @@ def lnprob(params, *args):
     mod: (list)
         list of pre-computed star model objects.
     """
-
-    # Figure out whether the iso or gyro or both likelihoods should be called.
+    # print(lnlike(params, *args), lnprior(params), "lnlike, lnprior")
     return lnlike(params, *args) + lnprior(params)
 
 
@@ -128,11 +124,12 @@ def MH(par, lnprob, nsteps, t, *args):
     samples = np.zeros((nsteps, len(par[par_inds])))
     accept, probs = 0, []
     for i in range(nsteps):
-        par[par_inds], new_prob, acc = MH_step(par[par_inds], lnprob, t,
-                                               *args)
+        par[par_inds], new_prob, acc = MH_step(par[par_inds], lnprob,
+                                               t[par_inds], *args)
         accept += acc
         probs.append(new_prob)
         samples[i, :] = par[par_inds]
+    print(accept, nsteps)
     print("Acceptance fraction = ", accept/float(nsteps))
     return samples, par, probs
 
@@ -140,7 +137,7 @@ def MH(par, lnprob, nsteps, t, *args):
 def MH_step(par, lnprob, t, *args):
     newp = par + np.random.randn(len(par))*t
     new_lnprob = lnprob(newp, *args)
-    alpha = np.exp(new_lnprob)/np.exp(lnprob(par, *args))
+    alpha = np.exp(new_lnprob - lnprob(par, *args))
     if alpha > 1:
         par = newp*1
         accept = 1
@@ -155,18 +152,42 @@ def MH_step(par, lnprob, t, *args):
 
 
 def gibbs_control(par, mods, d, nsteps, niter, t):
+    """
+    This function tells the metropolis hastings what parameters to sample in.
+    params:
+    ------
+    par: (list)
+        The parameters.
+    mods: (list)
+        The list of starmodel objects.
+    d: (pandas.dataframe)
+        The data.
+    nsteps: (int)
+        Number of samples.
+    niter: (int)
+        The number of gibbs cycles to perform.
+    t: (float)
+        The std of the proposal distribution.
+    returns:
+    -------
+    samples: (np.array)
+        2d array of samples. (nsteps, ndim)
+    lnprobs: (np.array)
+        Array of lnprobs
+    """
 
     N, ndim = len(d.age.values), len(par)
-    n_parameter_sets = N + 2  # Num conditionally independent param sets
+    n_parameter_sets = N + 1 + 1  # Number of param sets, including all.
 
     # Final sample array
     all_samples = np.zeros((nsteps * n_parameter_sets * niter, ndim))
 
-    par_inds = np.arange(ndim)
-    gyro_par_inds = np.concatenate((par_inds[:3], par_inds[3+N:3+2*N]))
+    # Construct parameter indices for the different parameter sets.
+    par_inds = np.arange(ndim)  # All
+    gyro_par_inds = np.concatenate((par_inds[:3], par_inds[3+N:3+2*N])) # Gyro
     iso_par_inds = []
     for i in range(N):
-        iso_par_inds.append(par_inds[3+i::N])
+        iso_par_inds.append(par_inds[3+i::N])  # Iso stars.
 
     args = [mods, d.period.values, d.period_err.values, d.bv.values,
             d.bv_err.values,
@@ -175,35 +196,43 @@ def gibbs_control(par, mods, d, nsteps, niter, t):
     # Iterate over niter cycles of parameter sets.
     _set, probs = 0, []
     for i in range(niter):
+        print("all")
+        print(par)
+        print("First likelihood evaluation...")
         # First sample all the parameters.
         args[-1] = par_inds
         samples, par, pb = MH(par, lnprob, nsteps, t, *args)
-        all_samples[nsteps*_set:nsteps*(_set+1), :][par_inds] = samples
+        all_samples[nsteps*_set:nsteps*(_set+1), par_inds] = samples
         probs.append(pb)
 
         # Then sample the gyro parameters only.
+        print("gyro")
+        print(par)
+        input("e")
         _set += 1
         args[-1] = gyro_par_inds
-        gyro_samples, last_gyro_sample, pb = run_MCMC(par, lnprob, nsteps, t,
-                                                      *args)
-        all_samples[nsteps*_set:(_set+1)*nsteps, :][gyro_par_inds] = \
+        gyro_samples, par, pb = MH(par, lnprob, nsteps, t, *args)
+        all_samples[nsteps*_set:(_set+1)*nsteps, gyro_par_inds] = \
             gyro_samples
         probs.append(pb)
 
         # Replace parameter array with the last sample from gyro.
-        par[gyro_par_inds] = last_gyro_sample
+        # par[gyro_par_inds] = last_gyro_sample
 
         # Then sample the stars, one by one.
         single_last_samps = []
         _set += 1
-        for i in range(nstars):
+        for i in range(N):
+            print("iso", i)
+            print(par)
+            input("e")
             args[-1] = iso_par_inds[i]
-            samps, last_samp, pb = run_MCMC(par, lnprob, nsteps, t, *args)
-            all_samples[_set*nsteps:(_set+1)*nsteps, :][iso_par_inds[i]] = \
+            samps, par, pb = MH(par, lnprob, nsteps, t, *args)
+            all_samples[_set*nsteps:(_set+1)*nsteps, iso_par_inds[i]] = \
                 samps
             probs.append(pb)
             _set += 1
-            par[iso_par_inds[i]] = last_samp
+            # par[iso_par_inds[i]] = last_samp
 
     lnprobs = np.array([i for j in probs for i in j])
     return all_samples, lnprobs
@@ -219,18 +248,14 @@ if __name__ == "__main__":
 
     start = time.time()  # timeit
 
-    t = [.01, .01, .01, .03, .1, .1, .3, .3, .3, .1, .2, .2, .02, .2, .2, .01,
-         .2, .2]
+    t = .5*np.array([.01, .01, .01, .03, .1, .1, .3, .3, .3, .1, .2, .2, .02,
+                     .2, .2, .01, .2, .2])
+    t = np.ones(len(t))*1e-3
     nsteps, niter = 1000, 3
     flat, lnprobs = gibbs_control(params, mods, d, nsteps, niter, t)
 
     end = time.time()
     print("Time taken = ", (end - start)/60, "minutes")
-
-    print("Making corner plot")
-    ndim = len(params)
-    fig = corner.corner(flat)
-    fig.savefig("corner_gibbs_for_realz")
 
     print("Plotting results and traces")
     plt.clf()
@@ -238,3 +263,8 @@ if __name__ == "__main__":
     plt.xlabel("Time")
     plt.ylabel("ln (probability)")
     plt.savefig("prob_trace")
+
+    print("Making corner plot")
+    ndim = len(params)
+    fig = corner.corner(flat)
+    fig.savefig("corner_gibbs_for_realz")
