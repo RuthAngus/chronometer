a99e1d6575a8fc2a25cf55cc1712d380b7eaf492
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-04-24 12:52:26 -0400

    braking it by switching to a different function

--- chronometer/test_gibbs.py
+++ chronometer/test_gibbs.py
@@ -15,8 +15,7 @@ def line_model(par, x):
 
 
 def gauss_model(par, x):
-    mu, sigma, A = np.exp(par)
-    return A*np.exp(-.5*((mu - x)/sigma)**2)
+    return np.exp(-.5*(x/np.exp(par))**2)
 
 
 def lnlike(par, x, y, yerr, par_inds):
@@ -38,13 +37,11 @@ def lnlike(par, x, y, yerr, par_inds):
 
 
 def emcee_lnlike(par, x, y, yerr):
-    line_lnlike = sum(-.5*((line_model(par[:2], x) - y)/yerr)**2)
-    gauss_lnlike = sum(-.5*((gauss_model(par[2:], x) - y)/yerr)**2)
-    # line_lnlike, gauss_lnlike = 0, sum(-.5*((gauss_model(par, x) - y)/yerr)**2)
-    # line_lnlike, gauss_lnlike = sum(-.5*((line_model(par, x) - y)/yerr)**2), 0
-    return line_lnlike + gauss_lnlike + lnprior(par)
-    # return = sum(-.5*((line_model(par[:2], x) + gauss_model(par[2:], x) -
-                   # y)/yerr)**2) + lnprior(par)
+    ll = sum(-.5*((line_model(par[:2], x) - y)/(yerr + np.exp(par[-1])))**2)
+    return ll + lnprior(par)
+    # line_lnlike = sum(-.5*((line_model(par[:2], x) - y)/yerr)**2)
+    # gauss_lnlike = sum(-.5*((0 - y)**2/(yerr + np.exp(par[-1]))**2))
+    # return line_lnlike + gauss_lnlike + lnprior(par)
 
 
 def lnprior(par):
@@ -57,22 +54,16 @@ def lnprior(par):
 
 def test_metropolis_hastings():
 
-    # c, m, mu, sigma, A
-    par = np.log(np.array([.7, 2.5, 2, .5, 10]))
-    # par = np.log(np.array([2, .5, 10]))
-    # par = np.log(np.array([.7, 2.5]))
+    par = np.log(np.array([.7, 2.5, 2]))
     print("log truth = ", par)
-    print("lin truth = ", np.exp(par))
+    print("truth = ", np.exp(par))
     par_init = par*1
 
     # Straight line model
     x = np.arange(0, 10, .1)
-    err = 2.
+    err = .1
     yerr = np.ones_like(x) * err
-    y = line_model(par[:2], x) + np.random.randn(len(x))*err + \
-        gauss_model(par[2:], x)
-    # y = np.random.randn(len(x))*err + gauss_model(par, x)
-    # y = np.random.randn(len(x))*err + line_model(par, x)
+    y = line_model(par[:2], x) + np.random.randn(len(x))*par[-1]
 
     # Plot the data.
     plt.clf()
@@ -81,9 +72,9 @@ def test_metropolis_hastings():
 
     print("Running Metropolis Hastings")
     nsteps, niter = 100000, 6  # N samples
-    t = np.array([.01, .01, .01, .01, .01])
-    par_ind_list = [np.array([0, 1, 2, 3, 4]), np.array([0, 1]),
-                    np.array([2, 3, 4])]
+    t = np.array([.01, .01, .01])
+    par_ind_list = [np.array([0, 1, 2]), np.array([0, 1]),
+                    np.array([2])]
 
     # args = np.array([x, y, yerr, par_ind_list[0]])
     # samples, lnprobs = gc.gibbs_control(par, lnlike, nsteps, niter, t,
@@ -93,8 +84,11 @@ def test_metropolis_hastings():
     nwalkers, nsteps, ndim = 64, 10000, len(par)
     p0 = [1e-4*np.random.rand(ndim) + par for i in range(nwalkers)]
     sampler = emcee.EnsembleSampler(nwalkers, ndim, emcee_lnlike, args=args)
+    print("burning in...")
+    pos, _, _ = sampler.run_mcmc(p0, nsteps)
+    sampler.reset()
     print("production run...")
-    sampler.run_mcmc(p0, nsteps)
+    sampler.run_mcmc(pos, nsteps)
     samples = np.reshape(sampler.chain, (nwalkers*nsteps, ndim))
 
     for i in range(len(par)):
@@ -118,16 +112,11 @@ def test_metropolis_hastings():
 
     plt.clf()
     plt.errorbar(x, y, yerr=yerr, fmt="k.")
-    plt.plot(x, line_model(results[:2], x) + gauss_model(results[2:], x))
-    plt.plot(x, line_model(par_init[:2], x) + gauss_model(par_init[2:], x))
-    # plt.plot(x, gauss_model(results, x))
-    # plt.plot(x, gauss_model(par_init, x))
-    # plt.plot(x, line_model(results, x))
-    # plt.plot(x, line_model(par_init, x))
+    plt.plot(x, line_model(results[:2], x) +
+             np.random.randn(len(x))*results[-1], ".")
     plt.savefig("test")
 
-    fig = corner.corner(samples, truths=par_init, labels=["c", "m", "mu",
-                                                          "std", "A"])
+    fig = corner.corner(samples, truths=par_init, labels=["c", "m", "std"])
     fig.savefig("corner_MH_test")
 
     plt.clf()
