7a984b3c3baddffbd6763cd7453bde2e167fcf38
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-05-25 11:06:05 -0400

    running emcee version

--- chronometer/chronometer.py
+++ chronometer/chronometer.py
@@ -30,6 +30,16 @@ plotpar = {'axes.labelsize': 18,
 plt.rcParams.update(plotpar)
 
 
+def get_n_things(mods, params):
+    """
+    Figure out number of stars, number of global params, etc.
+    """
+    N, nind = len(mods), 5
+    ngyro = 3
+    nglob = len(params) - (N*nind)
+    return N, ngyro, nglob, nind
+
+
 def lnlike(params, *args):
     """
     Probability of age and model parameters given rotation period and colour.
@@ -47,12 +57,19 @@ def lnlike(params, *args):
         The indices of the parameters to vary.
     """
     mods, period, period_errs, bv, bv_errs, par_inds = args
+    N, ngyro, nglob, nind = get_n_things(mods, params)
     gyro_lnlike, iso_lnlike = 0, 0
     if par_inds[0] == 0 and par_inds[1] == 1 and par_inds[2] == 2: # if gyro.
-        r = np.isfinite(period)
-        r_period, r_period_errs, r_bv = period[r], period_errs[r], bv[r]
-        gyro_lnlike = sum(-.5*((r_period - gyro_model(params, r_bv))
-                                /r_period_errs)**2)
+        # r = np.isfinite(period)
+        # r_period, r_period_errs, r_bv = period[r], period_errs[r], bv[r]
+        # gyro_lnlike = sum(-.5*((r_period - gyro_model(params, r_bv))
+                                # /r_period_errs)**2)
+        m = (bv > .4) * (np.isfinite(period))
+        par_inds_mask = np.concatenate((par_inds[:ngyro],
+                                        g_par_inds[ngyro:][m]))
+        gyro_lnlike = sum(-.5*((period[m] - gyro_model(params[par_inds_mask],
+                                                       bv[m]))
+                                /period_errs[m])**2)
 
     # If not gyro but single stars
     else:
@@ -71,23 +88,16 @@ def emcee_lnprob(params, *args):
     The lnprob function used for emcee sampling. Does not break parameters
     into sets.
     """
-    ngyro, nglob, N, mods, period, period_errs, bv, bv_errs, Jz, Jz_err, \
-        par_inds = args
+    N, ngyro, nglob, nind = get_n_things(args[0], params)
+    mods, period, period_errs, bv, bv_errs, Jz, Jz_err, par_inds = args
     age_par_inds = par_inds[nglob+N:nglob+2*N]
     g_par_inds = np.concatenate((par_inds[:ngyro],
                                  par_inds[nglob+N:nglob+2*N]))
     m = (bv > .4) * (np.isfinite(period))
     g_par_inds_mask = np.concatenate((g_par_inds[:3], g_par_inds[3:][m]))
-    print(g_par_inds_mask)
     gyro_lnlike = sum(-.5*((period[m] - gyro_model(params[g_par_inds_mask],
                                                      bv[m]))
                             /period_errs[m])**2)
-    print(bv, "bv")
-    print(period[m], "period")
-    print(-.5*((period[m] - gyro_model(params[g_par_inds], bv[m]))
-               / period_errs[m])**2, "not sum")
-    print(params[g_par_inds], "params")
-    print(gyro_model(params[g_par_inds_mask], bv[m]), "model")
     kin_lnlike = action_age(params[ngyro], params[age_par_inds], Jz, Jz_err)
     p = params*1
     p[nglob:nglob+N] = np.exp(p[nglob:nglob+N])
@@ -95,8 +105,8 @@ def emcee_lnprob(params, *args):
     p[nglob+nglob*N:nglob+4*N] = np.exp(p[nglob+nglob*N:nglob+4*N])
     iso_lnlike = sum([mods[i].lnlike(p[nglob+i::N]) for i in
                       range(len(mods))])
-    print(gyro_lnlike, iso_lnlike, kin_lnlike, lnprior(params, *args))
-    return gyro_lnlike + iso_lnlike + kin_lnlike + lnprior(params, *args)
+    return gyro_lnlike + iso_lnlike + kin_lnlike + \
+        emcee_lnprior(params, *args)
 
 
 def lnprior(params, *args):
@@ -134,6 +144,27 @@ def lnprior(params, *args):
     m = (-20 < params) * (params < 20)  # Broad bounds on all params.
 
     if sum(m) == len(m) and mAv:
+        return g_prior + age_prior + feh_prior + distance_prior
+    else:
+        return -np.inf
+
+
+def emcee_lnprior(params, *args):
+    """
+    lnprior on all parameters.
+    """
+    N, ngyro, nglob, nind = get_n_things(args[0], params)
+    g_prior = priors.lng_prior(params[:ngyro])
+    age_prior = sum([np.log(priors.age_prior(np.log10(1e9*np.exp(i))))
+                     for i in params[nglob+N:nglob+2*N]])
+    feh_prior = sum(np.log(priors.feh_prior(params[nglob+2*N:nglob+3*N])))
+    distance_prior = sum(np.log(priors.distance_prior(
+                            np.exp(params[nglob+3*N:nglob+4*N]))))
+    mAv = (0 <= params[nglob+4*N:nglob+5*N]) * \
+        (params[nglob+4*N:nglob+5*N] < 1)  # Prior on A_v
+    m = (-20 < params) * (params < 20)  # Broad bounds on all params.
+    mbeta = 0 < params[ngyro] < 1e20  # Prior on beta
+    if sum(m) == len(m) and sum(mAv) == len(mAv) and mbeta:
         return g_prior + age_prior + feh_prior + distance_prior
     else:
         return -np.inf
@@ -326,7 +357,7 @@ if __name__ == "__main__":
 
     # Use Metropolis hastings or emcee?
     run_MH = False
-    # RESULTS_DIR = "/Users/ruthangus/projects/chronometer/chronometer/MH"
+    # emcee_RESULTS_DIR = "/Users/ruthangus/projects/chronometer/chronometer/MH"
     RESULTS_DIR = "/Users/ruthangus/projects/chronometer/chronometer/emc"
 
     # Load the data for the initial parameter array.
@@ -336,7 +367,7 @@ if __name__ == "__main__":
 
     # Generate the initial parameter array and the mods objects from the data
     # The initial parameters
-    global_params = np.array([.7725, .601, .5189, 350.])  # a, b, n, beta
+    global_params = np.array([.7725, .601, .5189, np.log(350.)])  # a, b, n, beta
 
     params, mods = pars_and_mods(d, global_params)
     # params = find_optimum()
@@ -372,7 +403,7 @@ if __name__ == "__main__":
 
     # Sample posteriors using either MH gibbs or emcee
     if run_MH:
-        args = [mods, d.period.values, d.period_err.values, d.bv.values,
+        args = [mods, d.prot.values, d.prot_err.values, d.bv.values,
                 d.bv_err.values, par_inds_list]
         flat, lnprobs = gibbs_control(params, lnprob, nsteps, niter, ts,
                                     par_inds_list, args)
@@ -383,17 +414,15 @@ if __name__ == "__main__":
         flat = flat[burnin:, :]
 
     else:
-        emcee_args = [ngyro, nglob, N, mods, d.period.values,
-                      d.period_err.values, d.bv.values,
+        emcee_args = [mods, d.prot.values,
+                      d.prot_err.values, d.bv.values,
                       d.bv_err.values, d.Jz, d.Jz_err, par_inds]
-        nwalkers, nsteps, ndim = 64, 10000, len(params)
-        print(emcee_lnprob(params, *emcee_args))
+        nwalkers, nsteps, ndim = 64, 2000, len(params)
         p0 = [1e-4*np.random.rand(ndim) + params for i in range(nwalkers)]
-        assert 0
         sampler = emcee.EnsembleSampler(nwalkers, ndim, emcee_lnprob,
                                         args=emcee_args)
         print("burning in...")
-        pos, _, _ = sampler.run_mcmc(p0, 3000)
+        pos, _, _ = sampler.run_mcmc(p0, 500)
         sampler.reset()
         print("production run...")
         sampler.run_mcmc(pos, nsteps)
@@ -404,14 +433,19 @@ if __name__ == "__main__":
         data[:, :] = flat
         f.close()
 
-        emcee_labels = ["$a$", "$b$", "$n$", "$\\beta$", "$\ln(Mass_1)$",
-                        "$\ln(Mass_2)$", "$\ln(Mass_3)$", "$\ln(Age_{1,i})$",
-                        "$\ln(Age_{2,i})$", "$\ln(Age_{3,i})$", "$[Fe/H]_1$",
-                        "$[Fe/H]_2$", "$[Fe/H]_3$", "$\ln(D_1)$",
-                        "$\ln(D_2)$", "$\ln(D_3)$", "$A_{v1}$", "$A_{v2}$",
-                        "$A_{v3}$", "$\ln(Age_{1,g})$", "$\ln(Age_{2,g})$",
-                        "$\ln(Age_{3,g})$"]
-        fig = corner.corner(flat, truths=truths, labels=labels)
+        emcee_labels = ["$a$", "$b$", "$n$", "$\\beta$",
+                        "$\ln(Mass_1)$", "$\ln(Mass_2)$", "$\ln(Mass_3)$",
+                        "$\ln(Mass_4)$", "$\ln(Mass_5)$",
+                        "$\ln(Age_{1,i})$", "$\ln(Age_{2,i})$",
+                        "$\ln(Age_{3,i})$", "$\ln(Age_{4,i})$",
+                        "$\ln(Age_{5,i})$",
+                        "$[Fe/H]_1$", "$[Fe/H]_2$", "$[Fe/H]_3$",
+                        "$[Fe/H]_4$", "$[Fe/H]_5$",
+                        "$\ln(D_1)$", "$\ln(D_2)$", "$\ln(D_3)$",
+                        "$\ln(D_4)$", "$\ln(D_5)$",
+                        "$A_{v1}$", "$A_{v2}$", "$A_{v3}$",
+                        "$A_{v4}$", "$A_{v5}$"]
+        fig = corner.corner(flat, labels=emcee_labels)
         fig.savefig(os.path.join(RESULTS_DIR, "emcee_corner"))
         assert 0
 
