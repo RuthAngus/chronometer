3b23f97185e980da5c3cb35b9421fc7bd79b8aad
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-06-01 13:24:25 -0400

    not using NGC 2019

--- chronometer/make_data_file.py
+++ chronometer/make_data_file.py
@@ -25,17 +25,12 @@ def create_and_save_df(fn, *args):
     df.to_csv(os.path.join(DATA_DIR, fn))
 
 
-if __name__ == "__main__":
-    DATA_DIR = "/Users/ruthangus/projects/chronometer/chronometer/data"
-    sun = pd.read_csv(os.path.join(DATA_DIR, "sun.csv"))
-    NGC2019 = pd.read_csv(os.path.join(DATA_DIR, "NGC2019.csv"))
-    tgas = pd.read_csv(os.path.join(DATA_DIR, "kplr_tgas_periods.csv"))
-    tgas = tgas.iloc[:2, :]
-
-    # create_and_save_df(sun, NGC2019)
-    fn = "data.csv"
-    create_and_save_df(fn, sun, NGC2019, tgas)
-    df = pd.read_csv(os.path.join(DATA_DIR, fn))
+def add_actions_and_colours(df, fn):
+    """
+    Given a pandas dataframe (of the kepler and TGAS intersection), calculate
+    B-V colours and actions and add these properties to the dataset.
+    fn is the name of the file to save as, including the path.
+    """
 
     # Calculate B-Vs
     bv = tbv.teff2bv(df.teff.values, df.logg.values, df.feh.values)
@@ -43,7 +38,24 @@ if __name__ == "__main__":
 
     # Calculate actions
     action_array = convert_coords_to_actions(df)
-    save_pd_file(df, os.path.join(DATA_DIR, "action_data.csv"), action_array)
+    save_pd_file(df, fn, action_array)
 
     print(df["prot"])
     print(df["tgas_parallax"])
+
+if __name__ == "__main__":
+    DATA_DIR = "/Users/ruthangus/projects/chronometer/chronometer/data"
+
+    # Load dataframes for data you want to combine
+    sun = pd.read_csv(os.path.join(DATA_DIR, "sun.csv"))
+    NGC2019 = pd.read_csv(os.path.join(DATA_DIR, "NGC2019.csv"))
+    tgas = pd.read_csv(os.path.join(DATA_DIR, "kplr_tgas_periods.csv"))
+    tgas = tgas.iloc[:100, :]
+    fn = "data.csv"
+
+    # Join data files together.
+    create_and_save_df(fn, sun, tgas)
+
+    # Calculate B-V colour and actions and append to data file.
+    df = pd.read_csv(os.path.join(DATA_DIR, fn))
+    add_actions_and_colours(df, os.path.join(DATA_DIR, "action_data.csv"))
